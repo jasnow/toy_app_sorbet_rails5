5802106f32207e152a6b20ead588cbee19656da5
Al Snow <jasnow@hotmail.com>
2016-07-20 18:23:28 -0400

    Upgraded to Rails 5.0

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20140928160317) do
 
-  create_table "microposts", force: true do |t|
+  create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
