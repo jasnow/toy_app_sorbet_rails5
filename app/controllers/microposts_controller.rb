761875da3a62e317bd341c69b94da8c971b154f2
Al Snow <jasnow@hotmail.com>
2017-12-02 10:58:01 -0500

    Fixed 'Unscoped Find' error

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -69,7 +69,7 @@ class MicropostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_micropost
-      @micropost = Micropost.find(params[:id])
+      @micropost = Micropost.where("id = ?", params[:id]).first
     end
 
     # Never trust parameters from the scary internet, only allow
